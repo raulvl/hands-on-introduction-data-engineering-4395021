a350a4250b68553fc498a26b5a401f0db7ce9783
Raúl Eduardo Villegas Leiva <raulvl@users.noreply.github.com>
2023-12-04 14:58:41 +0000

    ddd

--- airflow/dags/extract_dag.py
+++ airflow/dags/extract_dag.py
@@ -1,1 +1,16 @@
 
+''' Extract DAG '''
+from datetime import datetime
+from airflow.operators.bash import BashOperator
+from airflow import DAG
+
+with DAG('extract_dag',
+         schedule_interval=None,
+         start_date=datetime(2022,1,1),
+         catchup=False
+         ) as dag:
+
+    extract_task = BashOperator(
+        task_id='extract_task',
+        bash_command='wget -c https://datahub.io/core/top-level-domain_names/r/top-level-domain-names.csv.csv -O /workspaces/hands-on-introduction-data-engineering-4395021/lab/orchestrated/airflow-extract-data.csv'
+    )
